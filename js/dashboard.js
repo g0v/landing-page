5b83ad7ce236697dbcbc2fd7a81856bd30863cd7
SeanGau <rrtw0627@gmail.com>
2024-12-12 15:11:49 +0800

    regen for c026afa

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,24 +1,23 @@
-window.addEventListener('load', function(){
-  var xhr;
-  xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://dashboard.g0v.tw/', true);
-  xhr.onreadystatechange = function(){
-    var data, date, titles;
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      data = JSON.parse(xhr.responseText);
-      document.getElementById('loading-text').style.display = 'none';
-      date = new Date(data.create_at);
-      document.getElementById('update-time').querySelector('span').textContent = date.toLocaleString();
-      titles = Object.keys(data.data);
-      return titles.forEach(function(_title){
-        var _data, subtitles;
-        _data = data.data[_title];
-        subtitles = Object.keys(_data);
-        return subtitles.forEach(function(_subtitle){
-          return document.getElementById(_title + '-' + _subtitle).querySelector('.card-text span').textContent = _data[_subtitle];
-        });
+document.addEventListener('DOMContentLoaded', function(){
+  return fetch('https://dashboard.g0v.tw/').then(function(response){
+    return response.json();
+  }).then(function(data){
+    var date, titles;
+    document.getElementById('loading-text').style.display = 'none';
+    date = new Date(data.create_at);
+    document.querySelector('#update-time span').textContent = date.toLocaleString();
+    titles = Object.keys(data.data);
+    return titles.forEach(function(title){
+      var _data, subtitles;
+      _data = data.data[title];
+      subtitles = Object.keys(_data);
+      return subtitles.forEach(function(subtitle){
+        var element;
+        element = document.querySelector("#" + title + "-" + subtitle + " .card-text span");
+        return element != null ? element.textContent = _data[subtitle] : void 8;
       });
-    }
-  };
-  return xhr.send();
+    });
+  })['catch'](function(error){
+    return console.error('Error fetching data:', error);
+  });
 });
--- js/insights.js
+++ js/insights.js
@@ -1,14 +1,21 @@
-fetch('https://api.g0v.tw/hackmd/?tag=jothon').then(function(response){
+fetch('https://api.g0v.tw/hackmd/?tag=insights%20for%20g0v.tw').then(function(response){
   return response.json();
 }).then(function(data){
   console.log(data);
   return data.map(function(item, index){
-    var summary, row;
-    summary = item.summary.replace(/[*_`]/g, '');
+    var summary, tags, row;
+    console.log(item);
+    summary = item.summary.content.replace(/[*_`]/g, '');
     summary = summary.substring(0, 100);
+    tags = {};
+    item.summary.tags.map(function(tag){
+      return tags[tag[0]] = tag[1];
+    });
     row = document.createElement('div');
     row.className = 'col-12 col-sm-6 col-lg-4 p-2';
-    row.innerHTML = "<div class=\"card\">\n  <div class=\"card-body\">\n    <h5 class=\"card-title\">" + item.title + "</h5>\n    <p class=\"card-text\">" + summary + "</p>\n  </div>\n</div>";
+    row.innerHTML = "<div class=\"card\">\n  <div class=\"card-body\">\n    <h5 class=\"card-title\">" + item.title + "</h5>\n    " + (tags.author ? "<div class=\"mt-2 mt-3 fs-6 text-secondary\">" + tags.author + "</div>" : '') + "\n    <p class=\"card-text\">" + summary + "</p>\n  </div>\n</div>";
     return document.getElementById('insights-rows').appendChild(row);
   });
-})['catch'](function(error){});
+})['catch'](function(error){
+  return console.log(error);
+});
